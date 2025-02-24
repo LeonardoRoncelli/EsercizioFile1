5c812ca0f6fb1e4d374803ab9d8985f549b911c0
RONCELLI.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-02-24 12:45:04 +0100

    Metodi set e get classe Persona.

--- src/Persona.java
+++ src/Persona.java
@@ -14,4 +14,76 @@ public class Persona {
         this.dataIscrizione=dataIscrizione;
         this.sito=sito;
     }
+    public int getIndice() {
+        return indice;
+    }
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public String getCompagnia() {
+        return compagnia;
+    }
+    public void setCompagnia(String compagnia) {
+        this.compagnia = compagnia;
+    }
+    public String getCitta() {
+        return citta;
+    }
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+    public String getNazione() {
+        return nazione;
+    }
+    public void setNazione(String nazione) {
+        this.nazione = nazione;
+    }
+    public String getTelefono1() {
+        return telefono1;
+    }
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+    public String getTelefono2() {
+        return telefono2;
+    }
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+    public String getDataIscrizione() {
+        return dataIscrizione;
+    }
+    public void setDataIscrizione(String dataIscrizione) {
+        this.dataIscrizione = dataIscrizione;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSito() {
+        return sito;
+    }
+    public void setSito(String sito) {
+        this.sito = sito;
+    }
 }
